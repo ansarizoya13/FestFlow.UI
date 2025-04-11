0f8e5315275de743dc1c1819a0782c8bc301da64
ansarizoya13 <153110724+ansarizoya13@users.noreply.github.com>
2025-04-12 00:53:28 +0530

    Updated environment.ts
    Made API URL from http to https

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production : false,
     //apiUrl : "http://localhost:5050"
-    apiUrl : "http://festflowai.runasp.net"
+    apiUrl : "https://festflowai.runasp.net"
 }
